7b81dace4ec645e4cadb7cd02f95273749dfe67b
Bruno Dobrovolski <brunomanoel@dobrovolski.com.br>
2022-08-08 10:06:32 -0600

    feat: code planning and organization

--- source/void_reconstruction.py
+++ source/void_reconstruction.py
@@ -92,20 +92,102 @@ def find_voids(pa_pic, maxarea):
 
 
 def open_gb_files(path: str):
-    os.listdir
-    sample1 = np.loadtxt(path + "/data/1_001.txt")
+    
+    samples_list = os.listdir(path)    
+    
+    return samples_list
+
+
+import numpy as np
+import pandas as pd
+from matplotlib import pyplot as plt
+from skimage import draw
+
+def gb_reconstruction(sample: str):
+    
+    sample = np.loadtxt(sample + ".txt")
+    
+    '''
+    # Column 1-3:   right hand average orientation (phi1, PHI, phi2 in radians)
+    # Column 4-6:   left hand average orientation (phi1, PHI, phi2 in radians)
+    # Column 7:     Misorientation Angle
+    # Column 8-10:  Misorientation Axis in Right Hand grain
+    # Column 11-13: Misorientation Axis in Left Hand grain
+    # Column 14:    length (in microns)
+    # Column 15:    trace angle (in degrees)
+    # Column 16-19: x,y coordinates of endpoints (in microns)
+    # Column 20-21: IDs of right hand and left hand grains
+    
+    '''
+    
+    
+    df = pd.DataFrame(  data = sample, 
+                        columns = ["right_phi1","right_PHI","right_phi2",                 #1-3
+                                   "left_phi1","left_PHI","left_phi2",                    #4-6 
+                                   "ori_angle",                                           #7
+                                   "right_ori_x","right_ori_y","right_ori_z",              #8-10
+                                   "left_ori_x","leff_ori_y","left_ori_z",                 #11-13  
+                                   "length",                                              #14
+                                   "trace_angle",                                         #15
+                                   "x_start", "y_start", "x_end", "y_end",                #16-19
+                                   "grain_right","grain_left"                             #20-21
+                                   ]                    
+                     )
+
+    
+        
+    width = int(df.x_end.max())
+    height = int(df.y_end.max())
+    
+    coordinates = df[["x_start","y_start","x_end","y_end"]]
+    coordinates_array = coordinates.to_numpy()
+    
+    coordinates_array = (coordinates_array.astype(int))
+    
+    rows = coordinates_array.shape[0]
+    
+    
+    '''
+
+        Drawing the image using numpy and scikit
+        
+    '''
+    
+    draw_image()
+
+    os.mkdir("output")
+
+    
+    
     return 0
 
-def gb_reconstruction():
-    pass
-    return 0
+
+def draw_image(width: int, height: int ):
+    
+    np_img = np.zeros((width+1,height+1))
+    for x in range(rows):
+        rr,cc = draw.line(coordinates_array[x][0],coordinates_array[x][1],coordinates_array[x][2],coordinates_array[x][3])
+        np_img[rr,cc] = 1
+
+    #rr,cc = draw.line(0,0,100,100)
+    #np_img = np.rot90(np_img)
+    np_img = np.flip(np_img)
+    plt.imshow(np_img,cmap= "Greys")
+     
+    #img2 = Image.fromarray(np_img*255)
+    #img2.show()
+
 
 import os
+
 def main():
     path = os.getcwd()
-    open_gb_files(path)
-    gb_reconstruction()
-    find_voids()
+    samples_list = open_gb_files(path + "/data/")
+    for sample in samples_list:        
+        gb_reconstruction(sample)
+        find_voids(sample)
+    
+        
     return 0
 
 if __name__ == "__main__":
