6f69a4bd37e6390ac66510bdf05d55d981073df9
Bruno Dobrovolski <brunomanoel@dobrovolski.com.br>
2022-08-31 11:30:45 -0600

    fix: voids position detection

--- source/GB_reconstruction.py
+++ source/GB_reconstruction.py
@@ -26,6 +26,8 @@ def find_voids(pa_pic, maxarea):
 	### Voids will be more of a round shape this way, which reduces error.
 	### Depending on grain structure, some filters may have no effect on particular samples.
     original = cv2.bilateralFilter(original,9,75,75)
+    
+    #%%
 	#original = cv2.medianBlur(original,5)
 	#original = cv2.GaussianBlur(original,(5,5),0)
 	
@@ -33,9 +35,9 @@ def find_voids(pa_pic, maxarea):
 	### Determine treshold for binary image. First value is gray-value used for cut-off, second is white.
     retval, image = cv2.threshold(original, 60, 255, cv2.THRESH_BINARY)
 
-	
+	#%%
 	### Find eliptical shapes with a minimum size and dilate picutre
-    el = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (1, 1))
+    #el = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (1, 1))
     #image = cv2.dilate(image, el, iterations=1)
 
 	### Save picture temporarily for comparison
@@ -51,7 +53,7 @@ def find_voids(pa_pic, maxarea):
 	
 	### Load bakcground image for contour plot
     drawing = cv2.imread(pa_pic)
-
+#%%
 	### Get height of the picture to set up an approximate area (height*height)
     vheight = len(drawing)
 	
@@ -105,6 +107,100 @@ def find_voids(pa_pic, maxarea):
     	
     return centers, radii, vheight, image, drawing
 
+#%%
+
+
+def find_voids_2(pa_pic, maxarea):
+	### Read image
+    
+    original = pa_pic
+	
+	### Save temp file for comparison
+	#cv2.imwrite(picname+"_original.png", original)
+	
+	### Run several filter over image to achieve a more distinct void structure.
+	### Voids will be more of a round shape this way, which reduces error.
+	### Depending on grain structure, some filters may have no effect on particular samples.
+    original = cv2.bilateralFilter(original,9,75,75)
+    
+	#original = cv2.medianBlur(original,5)
+	#original = cv2.GaussianBlur(original,(5,5),0)
+	
+	### TO CHANGE:
+	### Determine treshold for binary image. First value is gray-value used for cut-off, second is white.
+    retval, image = cv2.threshold(original, 60, 255, cv2.THRESH_BINARY)
+
+	
+	### Find eliptical shapes with a minimum size and dilate picutre
+    #el = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (1, 1))
+    #image = cv2.dilate(image, el, iterations=1)
+
+	### Save picture temporarily for comparison
+	#cv2.imwrite(pa_pic+"_dilated.png", image)
+	#cv2.imshow('dilated', image)
+	
+	### Read contours from dilated binary picture
+    contours, hierarchy = cv2.findContours(image,
+	    cv2.RETR_LIST,
+	    cv2.CHAIN_APPROX_SIMPLE
+	) ### Simplify contour 
+
+	
+	### Load bakcground image for contour plot
+    drawing = pa_pic
+
+	### Get height of the picture to set up an approximate area (height*height)
+    vheight = len(drawing)
+	
+    centers = []
+    radii = []
+	
+    for contour in contours:
+        area = cv2.contourArea(contour)
+		### there is one contour that contains all others (perimeter of image), filter it out
+		### do not run the rest of the loop, jump straight to next contour
+        if area > maxarea:
+                continue
+		
+		### bound contour with a rectangle. 	
+        br = cv2.boundingRect(contour)
+		
+		#el2 = cv2.fitEllipse(contour)
+
+		### TO CHANGE:
+		### Determine radius of circle by taking half of a side (which one??) 
+		### of the bounding Rectangle, then multiply by a factor if desired to make 
+		### up for mismatch due to color treshold in line 14.
+        radius = (br[2]/2)*1.4
+        radii.append(radius)
+		
+		### Find moments of countour
+        m = cv2.moments(contour)
+		
+		### Avoid error due to division by zero
+        if m['m00'] == 0.0:
+            m['m00'] = 1.0
+					
+        center = (int(m['m10'] / m['m00']), int(m['m01'] / m['m00']))
+        centers.append(center)
+		#cv2.circle(drawing, center, 3, (255, 0, 0), -1)
+        cv2.circle(drawing, center, int(radius), (0, 0, 255), 3)
+	
+    print("The program has detected {} voids".format(len(centers)))
+	
+    i = 0
+    for center in centers:
+        cv2.circle(drawing, center, 3, (255, 0, 0), -1)
+        cv2.circle(drawing, center, int(radii[i]), (0, 255, 0), 1)
+        i = i + 1
+
+	### Save image with recognized voids
+   # cv2.imwrite("_drawing.png", drawing)
+   # cv2.imshow('finished', drawing)
+   # cv2.waitKey()
+   # cv2.destroyAllWindows()
+    	
+    return centers, radii, vheight, image, drawing
 
 
 
@@ -166,7 +262,9 @@ np_img = np.zeros([height+1, width+1, 3])
 for idx, row in enumerate(coordinates_array):
     rr,cc = draw.line(row[0],row[1],row[2],row[3])
 #    np_img[cc,rr,0:2] = [255, idx/3934*255]
-    np_img[cc,rr,0:2] = [255, idx]
+    
+#    np_img[cc,rr,0:2] = [1, idx/3934]
+    np_img[cc,rr,0:2] = [1, idx]
 
 
 #np_img = np.transpose(np_img)
@@ -183,16 +281,14 @@ plt.imshow(np_img)
 
 '''
 
-    Drawing in jpg file using pillow
+    Save in jpg file using pillow
     
 '''
 path = '../data/'
 sample = "1_001.jpg"
-img_jpg = Image.open("../data/1_001.jpg")
+img_jpg = Image.open(path+sample)
 img_jpg = img_jpg.resize((width,height))
 
-
-#img.show()
 
 img_jpg1 = ImageDraw.Draw(img_jpg)
 
@@ -205,16 +301,26 @@ for row in coordinates_array:
 #img.save(fp = "./001", format = "png")
 img_jpg.show()
 #%%
-centers, radii, vheight, image, drawing = find_voids(path + sample,3000)
+
+
+gb_img = cv2.imread(path+sample, 0)
+gb_img = cv2.resize(gb_img,(width,height),interpolation = cv2.INTER_AREA)
+
+
+
+#centers, radii, vheight, image, drawing = find_voids('temp.jpg',3000)
+#centers, radii, vheight, image, drawing = find_voids(path + sample ,3000)
+centers, radii, vheight, image, drawing = find_voids_2(gb_img,3000)
 
 plt.imshow(drawing)
 
 #%%
 
+
 cv2.imwrite('temp.jpg',np_img)
 #img = cv2.cvtColor(np_img.astype('uint16'), cv2.COLOR_GRAY2BGR)
 img = cv2.imread('temp.jpg')
-os.remove("temp.jpg")
+#os.remove("temp.jpg")
 #cv2.waitKey()
 for center, rad in zip(centers, radii):
     cv2.circle(img,center,int(rad),(255,5,255), 5)
@@ -223,5 +329,6 @@ for center, rad in zip(centers, radii):
 cv2.imshow("image",img)
 cv2.waitKey()
 cv2.destroyAllWindows()
+
 
 
